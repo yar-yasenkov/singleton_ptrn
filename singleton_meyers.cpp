ada183968b85c1b454d4fb28849afae3dfe3ccb9
yar-yasenkov <yar-yasenkov@yandex.ru>
2016-12-12 10:43:08 +0300

    Create singleton_meyers.cpp

---
+++ singleton_meyers.cpp
@@ -0,0 +1,12 @@
+class Singleton
+{
+private: 
+    Singleton() {}
+    Singleton( const Singleton&);  
+    Singleton& operator=( Singleton& );
+public:
+    static Singleton& getInstance() {
+        static Singleton  instance;
+        return instance;
+    }    
+}; 
